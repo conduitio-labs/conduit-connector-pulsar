9391410c515e1dc775178c19a087c5d23ff306c7
Guillem <you@example.com>
2023-12-10 13:30:15 +0100

    Fill Destination methods

--- destination.go
+++ destination.go
@@ -6,11 +6,14 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/apache/pulsar-client-go/pulsar"
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
 type Destination struct {
 	sdk.UnimplementedDestination
+
+	producer pulsar.Producer
 
 	config DestinationConfig
 }
@@ -18,32 +21,23 @@ type Destination struct {
 type DestinationConfig struct {
 	// Config includes parameters that are the same in the source and destination.
 	Config
+
+	URL   string `json:"URL" validate:"required"`
+	Topic string `json:"topic" validate:"required"`
+
 	// DestinationConfigParam must be either yes or no (defaults to yes).
 	DestinationConfigParam string `validate:"inclusion=yes|no" default:"yes"`
 }
 
 func NewDestination() sdk.Destination {
-	// Create Destination and wrap it in the default middleware.
 	return sdk.DestinationWithMiddleware(&Destination{}, sdk.DefaultDestinationMiddleware()...)
 }
 
 func (d *Destination) Parameters() map[string]sdk.Parameter {
-	// Parameters is a map of named Parameters that describe how to configure
-	// the Destination. Parameters can be generated from DestinationConfig with
-	// paramgen.
 	return d.config.Parameters()
 }
 
 func (d *Destination) Configure(ctx context.Context, cfg map[string]string) error {
-	// Configure is the first function to be called in a connector. It provides
-	// the connector with the configuration that can be validated and stored.
-	// In case the configuration is not valid it should return an error.
-	// Testing if your connector can reach the configured data source should be
-	// done in Open, not in Configure.
-	// The SDK will validate the configuration and populate default values
-	// before calling Configure. If you need to do more complex validations you
-	// can do them manually here.
-
 	sdk.Logger(ctx).Info().Msg("Configuring Destination...")
 	err := sdk.Util.ParseConfig(cfg, &d.config)
 	if err != nil {
@@ -53,23 +47,42 @@ func (d *Destination) Configure(ctx context.Context, cfg map[string]string) erro
 }
 
 func (d *Destination) Open(ctx context.Context) error {
-	// Open is called after Configure to signal the plugin it can prepare to
-	// start writing records. If needed, the plugin should open connections in
-	// this function.
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL: d.config.URL,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create client: %w", err)
+	}
+
+	d.producer, err = client.CreateProducer(pulsar.ProducerOptions{
+		Topic: d.config.Topic,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create producer: %w", err)
+	}
+
 	return nil
 }
 
 func (d *Destination) Write(ctx context.Context, records []sdk.Record) (int, error) {
-	// Write writes len(r) records from r to the destination right away without
-	// caching. It should return the number of records written from r
-	// (0 <= n <= len(r)) and any error encountered that caused the write to
-	// stop early. Write must return a non-nil error if it returns n < len(r).
-	return 0, nil
+	var written int
+	for _, record := range records {
+		_, err := d.producer.Send(ctx, &pulsar.ProducerMessage{
+			Payload: record.Payload.After.Bytes(),
+		})
+		if err != nil {
+			return written, fmt.Errorf("failed to send message: %w", err)
+		}
+		written++
+	}
+
+	return written, nil
 }
 
 func (d *Destination) Teardown(ctx context.Context) error {
-	// Teardown signals to the plugin that all records were written and there
-	// will be no more calls to any other function. After Teardown returns, the
-	// plugin should be ready for a graceful shutdown.
+	if d.producer != nil {
+		d.producer.Close()
+	}
+
 	return nil
 }
--- destination_test.go
+++ destination_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	apachepulsar "github.com/alarbada/conduit-connector-apachepulsar"
+	"github.com/apache/pulsar-client-go/pulsar"
+	sdk "github.com/conduitio/conduit-connector-sdk"
 	"github.com/matryer/is"
 )
 
@@ -13,4 +15,59 @@ func TestTeardown_NoOpen(t *testing.T) {
 	con := apachepulsar.NewDestination()
 	err := con.Teardown(context.Background())
 	is.NoErr(err)
+}
+
+func TestDestination_Integration(t *testing.T) {
+	is := is.New(t)
+
+	con := apachepulsar.NewDestination()
+	ctx := context.Background()
+
+	err := con.Configure(ctx, map[string]string{
+		"URL":          "pulsar://localhost:6650",
+		"topic":        "test",
+		"subscription": "test",
+	})
+	is.NoErr(err)
+
+	err = con.Open(ctx)
+	is.NoErr(err)
+
+	defer func() {
+		err := con.Teardown(ctx)
+		is.NoErr(err)
+	}()
+
+	_, err = con.Write(ctx, []sdk.Record{
+		{
+			Position:  []byte{},
+			Operation: 0,
+			Metadata:  map[string]string{},
+			Key:       nil,
+			Payload: sdk.Change{
+				Before: nil,
+				After:  sdk.RawData("example message"),
+			},
+		},
+	})
+	is.NoErr(err)
+
+	{ // consume the test message
+		client, err := pulsar.NewClient(pulsar.ClientOptions{
+			URL: "pulsar://localhost:6650",
+		})
+		is.NoErr(err)
+		defer client.Close()
+
+		consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+			Topic:            "test",
+			SubscriptionName: "test",
+		})
+		is.NoErr(err)
+
+		msg, err := consumer.Receive(context.Background())
+		is.NoErr(err)
+
+		is.Equal(msg.Payload(), []byte("example message"))
+	}
 }
