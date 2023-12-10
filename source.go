14622f9b0b1644dbc0530b4761dcbc0f2424ca3b
Guillem <you@example.com>
2023-12-10 12:00:03 +0100

    Fill Source methods

--- source.go
+++ source.go
@@ -4,46 +4,41 @@ package apachepulsar
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"sync"
 
+	"github.com/apache/pulsar-client-go/pulsar"
 	sdk "github.com/conduitio/conduit-connector-sdk"
 )
 
 type Source struct {
 	sdk.UnimplementedSource
 
-	config           SourceConfig
-	lastPositionRead sdk.Position //nolint:unused // this is just an example
+	consumer pulsar.Consumer
+	received []pulsar.Message
+	mx       *sync.Mutex
+
+	config SourceConfig
 }
 
 type SourceConfig struct {
-	// Config includes parameters that are the same in the source and destination.
 	Config
-	// SourceConfigParam is named foo and must be provided by the user.
-	SourceConfigParam string `json:"foo" validate:"required"`
+
+	URL          string `json:"URL" validate:"required"`
+	Topic        string `json:"topic" validate:"required"`
+	Subscription string `json:"subscription" validate:"required"`
 }
 
 func NewSource() sdk.Source {
-	// Create Source and wrap it in the default middleware.
-	return sdk.SourceWithMiddleware(&Source{}, sdk.DefaultSourceMiddleware()...)
+	return sdk.SourceWithMiddleware(&Source{mx: &sync.Mutex{}}, sdk.DefaultSourceMiddleware()...)
 }
 
 func (s *Source) Parameters() map[string]sdk.Parameter {
-	// Parameters is a map of named Parameters that describe how to configure
-	// the Source. Parameters can be generated from SourceConfig with paramgen.
 	return s.config.Parameters()
 }
 
 func (s *Source) Configure(ctx context.Context, cfg map[string]string) error {
-	// Configure is the first function to be called in a connector. It provides
-	// the connector with the configuration that can be validated and stored.
-	// In case the configuration is not valid it should return an error.
-	// Testing if your connector can reach the configured data source should be
-	// done in Open, not in Configure.
-	// The SDK will validate the configuration and populate default values
-	// before calling Configure. If you need to do more complex validations you
-	// can do them manually here.
-
 	sdk.Logger(ctx).Info().Msg("Configuring Source...")
 	err := sdk.Util.ParseConfig(cfg, &s.config)
 	if err != nil {
@@ -53,46 +48,63 @@ func (s *Source) Configure(ctx context.Context, cfg map[string]string) error {
 }
 
 func (s *Source) Open(ctx context.Context, pos sdk.Position) error {
-	// Open is called after Configure to signal the plugin it can prepare to
-	// start producing records. If needed, the plugin should open connections in
-	// this function. The position parameter will contain the position of the
-	// last record that was successfully processed, Source should therefore
-	// start producing records after this position. The context passed to Open
-	// will be cancelled once the plugin receives a stop signal from Conduit.
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL: s.config.URL,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create client: %w", err)
+	}
+
+	s.consumer, err = client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            s.config.Topic,
+		SubscriptionName: s.config.Subscription,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create consumer: %w", err)
+	}
+
 	return nil
 }
 
 func (s *Source) Read(ctx context.Context) (sdk.Record, error) {
-	// Read returns a new Record and is supposed to block until there is either
-	// a new record or the context gets cancelled. It can also return the error
-	// ErrBackoffRetry to signal to the SDK it should call Read again with a
-	// backoff retry.
-	// If Read receives a cancelled context or the context is cancelled while
-	// Read is running it must stop retrieving new records from the source
-	// system and start returning records that have already been buffered. If
-	// there are no buffered records left Read must return the context error to
-	// signal a graceful stop. If Read returns ErrBackoffRetry while the context
-	// is cancelled it will also signal that there are no records left and Read
-	// won't be called again.
-	// After Read returns an error the function won't be called again (except if
-	// the error is ErrBackoffRetry, as mentioned above).
-	// Read can be called concurrently with Ack.
-	return sdk.Record{}, nil
+	msg, err := s.consumer.Receive(ctx)
+	if err != nil {
+		return sdk.Record{}, fmt.Errorf("failed to receive message: %w", err)
+	}
+
+	s.mx.Lock()
+	s.received = append(s.received, msg)
+	s.mx.Unlock()
+
+	var position sdk.Position
+	var metadata sdk.Metadata
+	var key sdk.Data
+	var payload sdk.Data = sdk.RawData(msg.Payload())
+
+	return sdk.Util.Source.NewRecordCreate(
+		position,
+		metadata,
+		key,
+		payload,
+	), nil
 }
 
-func (s *Source) Ack(ctx context.Context, position sdk.Position) error {
-	// Ack signals to the implementation that the record with the supplied
-	// position was successfully processed. This method might be called after
-	// the context of Read is already cancelled, since there might be
-	// outstanding acks that need to be delivered. When Teardown is called it is
-	// guaranteed there won't be any more calls to Ack.
-	// Ack can be called concurrently with Read.
-	return nil
+func (s *Source) Ack(ctx context.Context, _ sdk.Position) error {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+
+	var err error
+	for _, msg := range s.received {
+		ackErr := s.consumer.Ack(msg)
+		if err != nil {
+			err = errors.Join(err, fmt.Errorf("failed to ack message: %w", ackErr))
+		}
+	}
+
+	return err
 }
 
 func (s *Source) Teardown(ctx context.Context) error {
-	// Teardown signals to the plugin that there will be no more calls to any
-	// other function. After Teardown returns, the plugin should be ready for a
-	// graceful shutdown.
+	s.consumer.Close()
 	return nil
 }
