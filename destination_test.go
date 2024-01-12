8b618bda28497cc047b57441595f814bac4e3a13
Guillem <you@example.com>
2024-01-12 17:02:42 -0300

    Improve test resiliency

--- destination_test.go
+++ destination_test.go
@@ -3,6 +3,7 @@ package apachepulsar_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	apachepulsar "github.com/alarbada/conduit-connector-apachepulsar"
 	"github.com/apache/pulsar-client-go/pulsar"
@@ -38,37 +39,49 @@ func TestDestination_Integration(t *testing.T) {
 		is.NoErr(err)
 	}()
 
-	go func () {
-		 // consume the test message
-		client, err := pulsar.NewClient(pulsar.ClientOptions{
-			URL: "pulsar://localhost:6650",
+	writeEndC := make(chan struct{})
+	go func() {
+		defer close(writeEndC)
+		_, err = con.Write(ctx, []sdk.Record{
+			{
+				Position:  []byte{},
+				Operation: 0,
+				Metadata:  map[string]string{},
+				Key:       nil,
+				Payload: sdk.Change{
+					Before: nil,
+					After:  sdk.RawData("example message"),
+				},
+			},
 		})
 		is.NoErr(err)
-		defer client.Close()
-
-		consumer, err := client.Subscribe(pulsar.ConsumerOptions{
-			Topic:            "destination_test",
-			SubscriptionName: "destination_test",
-		})
-		is.NoErr(err)
-
-		msg, err := consumer.Receive(context.Background())
-		is.NoErr(err)
-
-		is.Equal(msg.Payload(), []byte("example message"))
 	}()
 
-	_, err = con.Write(ctx, []sdk.Record{
-		{
-			Position:  []byte{},
-			Operation: 0,
-			Metadata:  map[string]string{},
-			Key:       nil,
-			Payload: sdk.Change{
-				Before: nil,
-				After:  sdk.RawData("example message"),
-			},
-		},
+	readExampleMsg(is)
+
+	select {
+	case <-writeEndC:
+		// Message has been correctly read
+	case <-time.After(10 * time.Second):
+		t.Fatal("Test timed out waiting for message consumption")
+	}
+}
+
+func readExampleMsg(is *is.I) {
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL: "pulsar://localhost:6650",
 	})
 	is.NoErr(err)
+	defer client.Close()
+
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            "destination_test",
+		SubscriptionName: "destination_test",
+	})
+	is.NoErr(err)
+
+	msg, err := consumer.Receive(context.Background())
+	is.NoErr(err)
+
+	is.Equal(msg.Payload(), []byte("example message"))
 }
--- source_test.go
+++ source_test.go
@@ -21,44 +21,43 @@ func TestSource_Integration(t *testing.T) {
 	is := is.New(t)
 
 	topic := "source_test"
-
-	go produceExampleMsg(is, topic)
-
-	t.Log("produced example message")
-
 	con := apachepulsar.NewSource()
 	ctx := context.Background()
-
 	err := con.Configure(ctx, map[string]string{
 		"URL":              "pulsar://localhost:6650",
-		"topic":			topic,
+		"topic":            topic,
 		"subscriptionName": "source_test",
 	})
 	is.NoErr(err)
 
-	t.Log("configured source")
-
 	err = con.Open(ctx, nil)
 	is.NoErr(err)
-
-	t.Log("opened source")
 
 	defer func() {
 		err := con.Teardown(ctx)
 		is.NoErr(err)
 	}()
 
-	t.Log("reading example message")
+	readEndC := make(chan struct{})
 
-	ctx, cancel := context.WithTimeout(ctx, 5 * time.Second)
-	defer cancel()
+	go func() {
+		defer close(readEndC)
 
-	record, err := con.Read(ctx)
-	is.NoErr(err)
-	is.Equal(string(record.Payload.After.Bytes()), "example message")
+		record, err := con.Read(ctx)
+		is.NoErr(err)
+		is.Equal(string(record.Payload.After.Bytes()), "example message")
+		err = con.Ack(ctx, record.Position)
+		is.NoErr(err)
+	}()
 
-	err = con.Ack(ctx, record.Position)
-	is.NoErr(err)
+	produceExampleMsg(is, topic)
+
+	select {
+	case <-readEndC:
+		// Message has been correctly read
+	case <-time.After(10 * time.Second):
+		t.Fatal("Test timed out waiting for message consumption")
+	}
 }
 
 func produceExampleMsg(is *is.I, topic string) {
