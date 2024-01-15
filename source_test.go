d288f63bb731fbf2f030cc5291553859bc4b7082
Guillem <you@example.com>
2024-01-15 19:52:08 -0300

    Add partial and full restart source reads

--- source_test.go
+++ source_test.go
@@ -2,10 +2,11 @@ package pulsar
 
 import (
 	"context"
-	"sync"
+	"fmt"
 	"testing"
 
 	"github.com/apache/pulsar-client-go/pulsar"
+	sdk "github.com/conduitio/conduit-connector-sdk"
 	"github.com/matryer/is"
 )
 
@@ -16,64 +17,124 @@ func TestTeardownSource_NoOpen(t *testing.T) {
 	is.NoErr(err)
 }
 
-func TestSource_Integration(t *testing.T) {
+func TestSource_Integration_RestartFull(t *testing.T) {
+	t.Parallel()
 	is := is.New(t)
 
-	topic := "source_test"
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		connectorSourceRead(is, topic)
-		wg.Done()
-	}()
-
-	go func() {
-		pulsarGoClientWrite(is, topic)
-		wg.Done()
-	}()
-
-	wg.Wait()
-}
-
-func connectorSourceRead(is *is.I, topic string) {
-	con := NewSource()
-	ctx := context.Background()
-	err := con.Configure(ctx, map[string]string{
+	topic := "pulsar.topic." + t.Name()
+	cfgMap := map[string]string{
 		"URL":              "pulsar://localhost:6650",
 		"topic":            topic,
-		"subscriptionName": "source_test",
-	})
-	is.NoErr(err)
+		"subscriptionName": topic + "-subscription",
+	}
 
-	err = con.Open(ctx, nil)
-	is.NoErr(err)
+	recs1 := generatePulsarMsgs(1, 3)
+	go producePulsarMsgs(is, topic, recs1)
+	lastPosition := testSourceIntegrationRead(is, cfgMap, nil, recs1, false)
 
-	record, err := con.Read(ctx)
-	is.NoErr(err)
-	is.Equal(string(record.Payload.After.Bytes()), "example message")
-	err = con.Ack(ctx, record.Position)
-	is.NoErr(err)
+	recs2 := generatePulsarMsgs(4, 6)
+	go producePulsarMsgs(is, topic, recs2)
+	testSourceIntegrationRead(is, cfgMap, lastPosition, recs2, false)
 }
 
-func pulsarGoClientWrite(is *is.I, topic string) {
+func TestSource_Integration_RestartPartial(t *testing.T) {
+	t.Parallel()
+	is := is.New(t)
+
+	topic := "pulsar.topic." + t.Name()
+	cfgMap := map[string]string{
+		"URL":              "pulsar://localhost:6650",
+		"topic":            topic,
+		"subscriptionName": topic + "-subscription",
+	}
+
+	recs1 := generatePulsarMsgs(1, 3)
+	go producePulsarMsgs(is, topic, recs1)
+
+	lastPosition := testSourceIntegrationRead(is, cfgMap, nil, recs1, true)
+
+	// only first record was acked, produce more records and expect to resume
+	// from last acked record
+	recs2 := generatePulsarMsgs(4, 6)
+	go producePulsarMsgs(is, topic, recs2)
+
+	var wantRecs []pulsar.ProducerMessage
+	wantRecs = append(wantRecs, recs1[1:]...)
+	wantRecs = append(wantRecs, recs2...)
+	testSourceIntegrationRead(is, cfgMap, lastPosition, wantRecs, false)
+}
+
+func generatePulsarMsgs(from, to int) []pulsar.ProducerMessage {
+	var msgs []pulsar.ProducerMessage
+
+	for i := from; i <= to; i++ {
+		msgs = append(msgs, pulsar.ProducerMessage{
+			Key:     fmt.Sprintf("test-key-%d", i),
+			Payload: []byte(fmt.Sprintf("test-key-%d", i)),
+		})
+	}
+
+	return msgs
+}
+
+func producePulsarMsgs(is *is.I, topic string, msgs []pulsar.ProducerMessage) {
 	client, err := pulsar.NewClient(pulsar.ClientOptions{
 		URL: "pulsar://localhost:6650",
 	})
 	is.NoErr(err)
-
 	defer client.Close()
 
 	producer, err := client.CreateProducer(pulsar.ProducerOptions{
 		Topic: topic,
 	})
 	is.NoErr(err)
-
 	defer producer.Close()
 
-	_, err = producer.Send(context.Background(), &pulsar.ProducerMessage{
-		Payload: []byte("example message"),
-	})
+	for _, msg := range msgs {
+		_, err = producer.Send(context.Background(), &msg)
+		is.NoErr(err)
+	}
+}
+
+// testSourceIntegrationRead reads and acks messages in range [from,to].
+// If ackFirst is true, only the first message will be acknowledged.
+// Returns the position of the last message read.
+func testSourceIntegrationRead(
+	is *is.I,
+	cfgMap map[string]string,
+	startFrom sdk.Position,
+	wantRecords []pulsar.ProducerMessage,
+	ackFirstOnly bool,
+) sdk.Position {
+	ctx := context.Background()
+
+	underTest := NewSource()
+	defer func() {
+		err := underTest.Teardown(ctx)
+		is.NoErr(err)
+	}()
+
+	err := underTest.Configure(ctx, cfgMap)
+	is.NoErr(err)
+	err = underTest.Open(ctx, startFrom)
 	is.NoErr(err)
 
+	var positions []sdk.Position
+	for _, wantRecord := range wantRecords {
+		rec, err := underTest.Read(ctx)
+		is.NoErr(err)
+		is.Equal(wantRecord.Key, string(rec.Key.Bytes()))
+
+		positions = append(positions, rec.Position)
+	}
+
+	for i, p := range positions {
+		if i > 0 && ackFirstOnly {
+			break
+		}
+		err = underTest.Ack(ctx, p)
+		is.NoErr(err)
+	}
+
+	return positions[len(positions)-1]
 }
