bee1448a0f8a867b1932bf001a058dc56629ec14
Guillem <someemail@dot.com>
2025-03-10 13:28:38 +0700

    increase expiration to 10 years

--- test/certs/setup-tls.sh
+++ test/certs/setup-tls.sh
@@ -2,7 +2,7 @@
 openssl genrsa -out ca.key.pem 2048
 
 # Create a CA certificate:
-openssl req -x509 -new -nodes -key ca.key.pem -subj "/CN=CARoot" -days 365 -out ca.cert.pem
+openssl req -x509 -new -nodes -key ca.key.pem -subj "/CN=CARoot" -days 3650 -out ca.cert.pem
 
 # Generate the server's private key:
 openssl genrsa -out broker.key.pem 2048
@@ -14,7 +14,7 @@ openssl pkcs8 -topk8 -inform PEM -outform PEM -in broker.key.pem -out broker.key
 openssl req -new -config ../broker.conf -key broker.key.pem -out broker.csr.pem -sha256
 
 # Sign server certificate with the CA:
-openssl x509 -req -in broker.csr.pem -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -out broker.cert.pem -days 365 -extensions v3_ext -extfile ../broker.conf -sha256
+openssl x509 -req -in broker.csr.pem -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -out broker.cert.pem -days 3650 -extensions v3_ext -extfile ../broker.conf -sha256
 
 # Generate the client's private key:
 openssl genrsa -out client.key.pem 2048
@@ -26,6 +26,6 @@ openssl pkcs8 -topk8 -inform PEM -outform PEM -in client.key.pem -out client.key
 openssl req -new -subj "/CN=client" -key client.key.pem -out client.csr.pem -sha256
 
 # Sign client certificate with the CA:
-openssl x509 -req -in client.csr.pem -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -out client.cert.pem -days 365 -sha256
+openssl x509 -req -in client.csr.pem -CA ca.cert.pem -CAkey ca.key.pem -CAcreateserial -out client.cert.pem -days 3650 -sha256
 
 chmod 644 *.pem
